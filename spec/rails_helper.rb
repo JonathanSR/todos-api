8362be4e8c43ea8b3853eab59a420f36509a8c8b
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-03-11 18:42:32 -0700

    adds controller functionality

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
+#require 'support/factory_girl'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -23,8 +24,11 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+RSpec.configure do |config|
+  config.include RequestSpecHelper, type: :request
+end
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
@@ -36,7 +40,7 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-RSpec.configuration do |config|
+RSpec.configure do |config| 
   # [...]
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
